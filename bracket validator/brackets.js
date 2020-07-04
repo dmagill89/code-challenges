eb2f6e2280e177b5c7363a78a6e6bf7ef409e3d2
David Magill <dmagill89@gmail.com>
2020-07-04 16:37:39 -0700

    format issue

--- bracket validator/brackets.js
+++ bracket validator/brackets.js
@@ -2,45 +2,44 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let stack = [];
-    let length = s?.length;
+var isValid = function (s) {
+  let stack = [];
+  let length = s?.length;
 
-    stack.push('0');
-    
-    for (let i = 0; i < length; i++) {
-        let c = s[i];
-        let popped = stack.pop();
+  stack.push('0');
 
-        if (isRight(c) && (popped === inverse(c))) {
-            continue;
-        } else if (isRight(c) && (popped !== inverse(c))) {
-            return false;
-        } else if (isLeft(c)) {
-            stack.push(popped);
-            leftStack.push(c);
-        }
-        
+  for (let i = 0; i < length; i++) {
+    let c = s[i];
+    let popped = stack.pop();
+
+    if (isRight(c) && (popped === inverse(c))) {
+      continue;
+    } else if (isRight(c) && (popped !== inverse(c))) {
+      return false;
+    } else if (isLeft(c)) {
+      stack.push(popped);
+      leftStack.push(c);
     }
-    
-    return true;
+  }
+
+  return true;
 };
 
-var inverse = function(c) {
-    switch (c) {
-        case ')':
-            return '(';
-        case '}':
-            return '{';
-        case ']':
-            return '[';
-    }
+var inverse = function (c) {
+  switch (c) {
+    case ')':
+      return '(';
+    case '}':
+      return '{';
+    case ']':
+      return '[';
+  }
 };
-    
-var isLeft = function(c) {
-    return (c === '(' || c === '{' || c === '[');
-}; 
 
-var isRight = function(c) {
-    return (c === ')' || c === '}' || c === '}');
+var isLeft = function (c) {
+  return (c === '(' || c === '{' || c === '[');
+};
+
+var isRight = function (c) {
+  return (c === ')' || c === '}' || c === '}');
 };
