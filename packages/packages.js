a17730fab5a22490f726b0f0292ca843adb97591
David Magill <dmagill89@gmail.com>
2020-07-05 14:59:31 -0700

    adding an interative approach to the packages problem

--- packages/packages.js
+++ packages/packages.js
@@ -32,6 +32,35 @@ function hasDependency(package, dependency) {
   return seen.has(dependency);
 }
 
+function hasDependencyIterative(package, dependency) {
+  const queue = [];
+  const seen = new Set();
+
+  queue.push(package);
+  while (queue.length > 0) {
+
+    const current = queue.shift();
+    if (!seen.has(current.name)) {
+      const dependencies = current.dependencies;
+      const numDependencies = dependencies.length;
+
+      seen.add(current.name);
+
+      for (let i = 0; i < numDependencies; i++) {
+        const dep = dependencies[i];
+
+        if (dep.name === dependency) {
+          return true;
+        } else if (!seen.has(dep.name)) {
+          queue.push(dep);
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
 // package = {
 //     name: 'A',
 //     dependencies: [
